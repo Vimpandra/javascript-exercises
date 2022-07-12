87726d4f027473ff2881b1a72124973cde948ef6
Vimpandra <victorimperatriz@gmail.com>
2022-07-11 23:06:55 -0300

    Solution to exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return `ERROR`
+    } else {    
+        let myStr = ``;
+        for (let i = 1; i <= num; i++) {
+            myStr += string;
+        }
+        return myStr
+    }
 };
 
 // Do not edit below this line
