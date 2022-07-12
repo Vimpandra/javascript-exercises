9b592fc19db4d315e1999869ebd3d14692056660
Vimpandra <victorimperatriz@gmail.com>
2022-07-12 05:43:40 -0300

    Solution to exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(base, top) {
+    let result = 0;
 
+    if (typeof(top) !== `number` || typeof(base) !== `number` || base < 0 || top < 0) {
+        return `ERROR`
+    } else {
+            if (base < top) {
+            for (let i = base; i <= top; i++) {
+                result += i;
+            }
+        } else {
+            for (let i = top; i <= base; i++) {
+                result += i;
+            }
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
