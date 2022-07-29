938d8d92e706507af00b8ed386b3c48d3debe29b
Vimpandra <victorimperatriz@gmail.com>
2022-07-29 20:36:52 -0300

    Solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let firstNum = 1;
+    let secondNum = 1;
+    if (n < 0) return `OOPS`;
+    else if (n === 0) return 0;
+    else if (n > 0) {
+        let a = 0;
+        let b = 1;
+        for (let i = 1; i < n; i++) {
+            const temp = b;
+            b = a + b;
+            a = temp;  
+        } return b;
+    }
+    
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
