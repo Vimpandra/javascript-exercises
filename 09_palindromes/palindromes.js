dd3a36f6c7a43fd9f4fd1a492aeea9ebe23f46b1
Vimpandra <victorimperatriz@gmail.com>
2022-07-29 19:29:12 -0300

    Solution to exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let noPunc = str.replace(/[.,\/#!$%\^&\*;:{}=\-_` ~()]/g,"").toLowerCase();
+    
+    let reverseStr = ``;
+    for (let i = noPunc.length - 1; i >= 0; i--) {
+        reverseStr += noPunc.substr(i, 1);
+    }
+
+    if (noPunc === reverseStr) return true;
+    else return false;
 
 };
 
