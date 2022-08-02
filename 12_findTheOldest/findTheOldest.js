b3d69aaee7706d7cdedbd3142b0fcb78175368f4
Vimpandra <victorimperatriz@gmail.com>
2022-08-02 00:10:01 -0300

    Solution to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let sortedAges = people.sort((a, b) => {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear();
+        } else if (!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear();
+        };
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+        if (aAge > bAge) return -1;
+        else return 1;
+    });
+    return sortedAges[0];
 };
 
 // Do not edit below this line
